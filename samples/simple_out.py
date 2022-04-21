93e2a176a0c14ff024ac3b15bc75f2ba50234a5b
Vijay Pothukuchi <vijayp@ti.com>
2022-04-21 15:39:09 -0500

    Updated the shebang to use python3.

--- samples/simple_out.py
+++ samples/simple_out.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
 # Copyright (c) 2021, Texas Instruments Incorporated. All rights reserved.
